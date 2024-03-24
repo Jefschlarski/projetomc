d9ad6a80fbab96848c267ad496bf5569388ebeb6
Jeferson <jefersonunik@gmai.com>
2024-03-24 01:53:22 -0300

    subindo projeto

---
+++ src/test/java/com/projetomc/projetomc/ProjetomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetomc.projetomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
